5e21581cfbf81a6cf4459304abb381609bf2a014
github username <github email address>
2018-01-23 08:26:41 -0500

    Footer

--- task_cheetah/db/schema.rb
+++ task_cheetah/db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180119200959) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
