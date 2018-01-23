99ccfa28812d283739e4e0d129d7a90d5f0e0a9e
github username <github email address>
2018-01-23 08:56:14 -0500

    Updated Errors and Category View

--- task_cheetah/app/models/user.rb
+++ task_cheetah/app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :jobs, :through => :tasks
   has_many :cats, :foreign_key => 'cheetah_id', :class_name => "Job"
 
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+
 
 
 end
