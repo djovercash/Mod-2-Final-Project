06e2bd0b92cbea6579a8fade45fccc89e747943b
github username <github email address>
2018-01-25 14:28:32 -0500

    New Seed Final

--- task_cheetah/db/seeds.rb
+++ task_cheetah/db/seeds.rb
@@ -31,33 +31,65 @@ avi = User.create(name: "Avi", username: "a789", password: "789", password_confi
 handy = Category.create(name: "Handyman")
 moving = Category.create(name: "Moving & Packing")
 furniture = Category.create(name: "Furniture Assembly")
-home = Category.create(name: "Home Improvement")
+tech = Category.create(name: "Technology")
 install = Category.create(name: "Mounting & Installation")
 yard = Category.create(name: "Yard Work")
 food = Category.create(name: "Food & Beverage")
 misc = Category.create(name: "Miscellaneous")
 
 bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help! Water EVERYWHERE!", snake_id: 1, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 100)
-wine = Task.create(title: "Wine Please", description: "I had a rough day. Please make it better with some Risling.", snake_id: 2, address: "11 Broadway Avenue, New York, NY 10004", cheetah_points: 10)
-fan = Task.create(title: "Ceiling Fan Installation", description: "Gurl, it is hot. Fix it with my ceiling fan.", snake_id: 6, address: "Time Square" , cheetah_points: 20)
-move = Task.create(title: "Gotta get out!", description: "Moving to UWS and need help packing and moving ASAP", snake_id: 3, address: "Harlem, New York", cheetah_points: 20)
-grass = Task.create(title: "Grass be out of control", description: "Please mow then lawn. No one wants this! PLEASE!!!", snake_id: 4, address: "Central Park", cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 5, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 6, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 7, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 8, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 9, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 10, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 11, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 12, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 13, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 14, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 15, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 16, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
-bathroom = Task.create(title: "Bathroom Explosion", description: "Pipes burst. Help Help Help!", snake_id: 17, address: "225 Fort Washington Ave., New York, NY, 10032" , cheetah_points: 20)
+wine = Task.create(title: "Wine Please", description: "I had a rough day. Please make it better with some Risling.", snake_id: 2, address: "Washington Heights", cheetah_points: 10)
+fan = Task.create(title: "Ceiling Fan Installation", description: "Gurl, it is hot. Fix it with my ceiling fan.", snake_id: 6, address: "Time Square" , cheetah_points: 40)
+move = Task.create(title: "Gotta get out!", description: "Moving to UWS and need help packing and moving ASAP", snake_id: 3, address: "Harlem, New York", cheetah_points: 80)
+grass = Task.create(title: "Grass be out of control", description: "Please mow then lawn. No one wants this! PLEASE!!!", snake_id: 4, address: "Central Park", cheetah_points: 10)
+ikea = Task.create(title: "Ikea Help", description: "My furniture arrived and I can't put it together. Anyone around to help?", snake_id: 5, address: "Battery Park" , cheetah_points: 45)
+burger = Task.create(title: "CHEESEBUURGERR", description: "I'm stuck at work and starving! Please get me a burger!", snake_id: 18, address: "11 Broadway Avenue, New York, NY 1004" , cheetah_points: 95)
+shelves = Task.create(title: "Shelves?", description: "So... I should be able to do this... but... Help me?", snake_id: 7, address: "Astoria, Queens" , cheetah_points: 25)
+packing = Task.create(title: "Spare Room Packing", description: "Making a life change and need to pack up my spare room.", snake_id: 8, address: "Upper West Side, New York", cheetah_points: 100)
+cat = Task.create(title: "Meow", description: "I need a cat sitter for tonight! Any takers?", snake_id: 9, address: "Hell's Kitchen, New York" , cheetah_points: 70)
+playground = Task.create(title: "Fort Builder", description: "Building an epic fort in my back yard. I need experienced Carpenters", snake_id: 10, address: "Hudson River Valley, New York" , cheetah_points: 200)
+heat = Task.create(title: "Heading Down. Ice Cold!", description: "I'm trying to fix my radiator and I can't figure it out. Someone out there to help?", snake_id: 11, address: "522 W. 157st, New York, NY 10032" , cheetah_points: 400)
+laptop = Task.create(title: "Why you so cruel?", description: "My Dell (I know) is really made at me. Anyone help???", snake_id: 12, address: "Brooklyn, New York" , cheetah_points: 64)
+tv = Task.create(title: "Television Color", description: "Last night my TV randomly turned black and white. Anyone with experience with televisions would be great! So much for a smart tv", snake_id: 13, address: "Wrigley Field" , cheetah_points: 60)
+shoe = Task.create(title: "Shoe Heels", description: "My favorite heels broke and I can't part with them. Anyone have cobbler experience?", snake_id: 14, address: "Willis Tower" , cheetah_points: 146)
+dog = Task.create(title: "Cutest Puppy Ever", description: "Can someone walk my dog? I've been called into a meeting", snake_id: 15, address: "Upper East Side, New York" , cheetah_points: 76)
+sad = Task.create(title: "Sad Day", description: "I need jokes. Please come make me laugh", snake_id: 16, address: "Chinatown, New York" , cheetah_points: 30)
+lights = Task.create(title: "Darkness Bad", description: "I've been lazy and my last light bulb burnt out. Can't someone help me out?", snake_id: 17, address: "Bronx, New York" , cheetah_points: 65)
 
 handy.tasks << bathroom
+handy.tasks << fan
+handy.tasks << ikea
+handy.tasks << shelves
+handy.tasks << playground
+handy.tasks << heat
+handy.tasks << shoe
+handy.tasks << lights
+
 food.tasks << wine
-home.tasks << fan
+food.tasks << burger
+
 moving.tasks << move
+moving.tasks << packing
+
+furniture.tasks << ikea
+furniture.tasks << fan
+furniture.tasks << tv
+
+tech.tasks << laptop
+tech.tasks << tv
+
+install.tasks << fan
+install.tasks << ikea
+install.tasks << shelves
+install.tasks << playground
+install.tasks << heat
+install.tasks << lights
+
 yard.tasks << grass
+yard.tasks << playground
+
+misc.tasks << cat
+misc.tasks << dog
+misc.tasks << wine
+misc.tasks << burger
+misc.tasks <<  shoe
